2d0f81f916491839edae6c002b6c64732adda054
BereketDan <bereketdaniel25@gmail.com>
2023-11-24 11:47:14 -0800

    Config urls and views for feedback

--- CSECdev/DevApp/urls.py
+++ CSECdev/DevApp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.base, name = 'base'),
     path('login/', views.login, name = 'login'),
-     path('index/', views.index, name = 'index'),
+    path('index/', views.index, name = 'index'),
+    path('feedback/', views.feedback, name = 'feedback'),
 ]
--- CSECdev/DevApp/views.py
+++ CSECdev/DevApp/views.py
@@ -14,3 +14,7 @@ def base(request):
 
 def index(request):
     return render(request, "index.html")
+
+def feedback(request):
+    return render(request, "feedback.html")
+
