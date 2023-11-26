117340a1d4fa3208558d4633b5fac751d69b22f1
BereketDan <bereketdaniel25@gmail.com>
2023-11-26 09:52:24 -0800

    remove incorrect funcation from view

--- CSECdev/DevApp/urls.py
+++ CSECdev/DevApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from . import views
+from . import views 
+
+
 
 
 urlpatterns = [
--- CSECdev/DevApp/views.py
+++ CSECdev/DevApp/views.py
@@ -3,8 +3,7 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def custom_404(request, exception):
-    return render(request, '404.html', status=404)
+
 
 def login(request):
     return render(request, "login.html")
