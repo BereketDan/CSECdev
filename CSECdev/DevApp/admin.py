6f7a219c13b9efc13d40902e4be42338cc14a79c
BereketDan <bereketdaniel25@gmail.com>
2023-11-27 18:26:48 -0800

    admin python file giving some space

--- CSECdev/DevApp/admin.py
+++ CSECdev/DevApp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import *
-admin.site.register(Member)# Register your models here.
-# 
+
+# Register your models here.
+
+admin.site.register(Member)
 admin.site.register(User_auth)
 admin.site.register(Event)
 admin.site.register(Feedback)
